5ac2886190fa029890ba096f94a0813cd91f2563
thang <nguyenthang011099.hust@gmail.com>
2019-10-09 08:15:31 +0700

    add js chart

---
+++ layvede.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd  "/home/thang/PHP/Simple_Dom/"
+php cr_bacgiang.php
+php cr_bacninh.php
+php cr_haiduong.php
+php cr_hanoi.php
+php cr_hoabinh.php
+php cr_hungyen.php
+php cr_phutho.php
+php cr_vinhphuc.php
+
+
+echo $(date): Done >> kttv.log
